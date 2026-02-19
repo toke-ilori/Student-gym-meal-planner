b76dd9a976d08235074606586293b3f27ecf44b7
tokeilori <tokeilori38@gmail.com>
2026-02-19 04:19:22 +0000

    feat: enhance dashboard UI with welcome card and navigation buttons

--- dashboard.php
+++ dashboard.php
@@ -11,28 +11,36 @@ require_once __DIR__ . '/includes/auth_check.php';
 
 </head>
 <body>
+    <div class="card">
     <h2>
         Welcome,
         <?php
-        echo htmlspecialchars($_SESSION['name']);
-        ?>
-        👋🏾
+        echo htmlspecialchars($_SESSION['name']);?> 👋🏾
     </h2>
 
-    <ul>
-        <!-- link to planner page -->
-        <a href="<?php echo BASE_URL; ?>/planner/planner.php">
-            Open planner
-        </a>
-        </li>
-        <!-- link to logout page -->
+    <p>
+        This is your personal Student Gym & Meal Planner.
+        Here you can track your workouts, plan your meals,
+        and stay consitent with your fitness goals.
+    </p>
+
+    <h3> What would you like to do today?</h3>
+
+    <ul class="dashboard-links">
         <li>
-            <a href="<?php echo BASE_URL; ?>/logout.php">
-                logout
+        <a class="btn" href="<?php echo BASE_URL; ?>/planner/planner.php">
+                🏋🏾 Open Planner
+            </a>
+        </li>
+
+        <li>
+            <a class="btn secondary" href="<?php echo BASE_URL; ?>/logout.php">
+                🚪 Logout
             </a>
         </li>
     </ul>
+
+</div>
+
 </body>
 </html>
-
-
